d817c8b20687e9bb214034f021eac42ede40867e
Asif Khan <beingasifk07@gmail.com>
2024-01-31 21:17:50 +0530

    first commit

---
+++ src/components/Transcription.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from "react";
+
+export default function Transcription(props) {
+  const { textElement } = props;
+
+  return <div>{textElement}</div>;
+}
